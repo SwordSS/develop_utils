cab977fd4e5eed89be906248e46fb7fe1b6cb005
SwordSS <869245512@qq.com>
2022-01-04 14:54:18 +0800

    增加了cmake关于策马可

--- cmake_demo/build/catkin_generated/setup_cached.sh
+++ cmake_demo/build/catkin_generated/setup_cached.sh
@@ -8,7 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/yyj/下载/develop_utils/cmake_demo/build/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/yyj/下载/develop_utils/cmake_demo/build/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/yyj/下载/develop_utils/cmake_demo/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH='/home/yyj/google_ws/install_isolated/bin:/opt/ros/kinetic/bin:/home/yyj/bin:/home/yyj/.local/bin:/opt/bros/bin:/home/yyj/.local/bin/:/home/yyj/bin:/home/yyj/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/yyj/下载/develop_utils/cmake_demo/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/yyj/下载/develop_utils/cmake_demo:$ROS_PACKAGE_PATH"
--- cmake_demo/src/main.cpp
+++ cmake_demo/src/main.cpp
@@ -7,5 +7,9 @@ using namespace std;
 int main()
 {
     OtherGroup all;
+    //调试用
+    int i = 0;
+    int j = 1; 
+    int k = 2;
     return 0;
 }
